9c5544ea7db0e387d6ef125ccc664fd5759025ff
zhanshenzhi2008 <lj2010vip@163.com>
2018-04-07 21:49:59 +0800

    '添加线程相关注解类'

---
+++ src/main/java/com/orjrs/demo/annoations/ThreadSafe.java
@@ -0,0 +1,18 @@
+package com.orjrs.demo.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记【线程安全】的类或写法
+ *
+ * @author orjrs
+ * @date 2018-04-0721:43
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String values() default "";
+}

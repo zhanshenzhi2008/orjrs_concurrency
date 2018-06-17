5d80a643d973802a7ad043e074ced734f90e92df
zhanshenzhi2008 <lj2010vip@163.com>
2018-06-17 12:07:03 +0800

    线程池

---
+++ src/main/java/com/orjrs/concurrency/action/threadpool/ThreadPoolDemo2.java
@@ -0,0 +1,29 @@
+package com.orjrs.concurrency.action.threadpool;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * newFixedThreadPool
+ *
+ * @author orjrs
+ * @date 2018-06-1018:27
+ */
+@Slf4j
+public class ThreadPoolDemo2 {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 50; i++) {
+            final int index = i;
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    log.info("task:{}", index);
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
